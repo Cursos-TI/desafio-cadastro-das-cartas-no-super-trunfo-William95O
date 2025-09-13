cfa875067cf4b177846862877e5205387f162deb
William95O <202509014771@alunos.estacio.br>
2025-09-12 21:16:32 -0300

    Update CartasSuperTrunfo.c
    Nivel Mestre - Criado o combate entre as cartas (Comparação entre os tributos de cada carta)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,24 +6,35 @@ int main()
     int numCarta1=1;
     char letra1[3];
     char cidade1[40];
-    int numPopulacao1;
+    unsigned long int numPopulacao1;
     float area1;
     float pib1;
     int pontoT1;
     float densPopulacional1;
     float pibperc1;
+    float super1;
 
 
     //variaveis Carta2
     int numCarta2=2;
     char letra2[3];
     char cidade2[40];
-    int numPopulacao2;
+    unsigned long int numPopulacao2;
     float area2;
     float pib2;
     int pontoT2;
     float densPopulacional2;
     float pibperc2;
+    float super2;
+
+    //Veriaveis comparadas
+    int numPopulacaoV;
+    int areV;
+    int pibV;
+    int pontoTV;
+    int densPopulacionalV;
+    int pibpercV;
+    int superV;
 
     //Informações da primeira carta
     printf("Digite a letra do estado da carta %i\n", numCarta1);
@@ -33,7 +44,7 @@ int main()
     scanf(" %[^\n]", cidade1);
 
     printf("Digite o numero da populacao da cidade de %s\n", cidade1);
-    scanf("%i", &numPopulacao1);
+    scanf("%lu", &numPopulacao1);
 
     printf("Informe a area total da cidade de %s\n", cidade1);
     scanf("%f", &area1);
@@ -45,6 +56,7 @@ int main()
     scanf("%i", &pontoT1);
 
     printf("\n");
+
     //Informações da segunda carta
     printf("Digite a letra do estado da carta %i\n", numCarta2);
     scanf("%s", letra2);
@@ -53,7 +65,7 @@ int main()
     scanf(" %[^\n]", cidade2);
 
     printf("Digite o numero da populacao da cidade de %s\n", cidade2);
-    scanf("%i", &numPopulacao2);
+    scanf("%lu", &numPopulacao2);
 
     printf("Informe a area total da cidade de %s\n", cidade2);
     scanf("%f", &area2);
@@ -72,6 +84,11 @@ int main()
     pibperc1 = (float)pib1/numPopulacao1;
     pibperc2 = (float)pib1/numPopulacao2;
 
+    //Calculando o SUPER PORDERRRRR
+    super1=numPopulacao1+area1+pib1+pontoT1+pibperc1+(1/densPopulacional1);
+    super2=numPopulacao2+area2+pib2+pontoT2+pibperc2+(1/densPopulacional2);
+
+
     // Exibição das cartas
     printf("\n\n");
     printf("=====RESPOSTAS=====\n");
@@ -86,6 +103,7 @@ int main()
     printf("Pontuacao Turistica: %i\n", pontoT1);
     printf("Densidade populacional: %f hab/Km2\n", densPopulacional1);
     printf("Pib per Capita: %f reais\n", pibperc1);
+    printf("SUPER PODER TOTAL: %.2f\n", super1);
 
     printf("\n\n");
 
@@ -99,6 +117,32 @@ int main()
     printf("Pontuacao Turistica: %i\n", pontoT2);
     printf("Densidade populacional: %f hab/Km2\n", densPopulacional2);
     printf("Pib per Capita: %f reais\n", pibperc2);
+    printf("SUPER PODER TOTAL: %.2f\n", super2);
+
+    printf("\n\n****COMBATE DAS CARTAS***\n");
+
+    printf("Vencedor do atributo POPULACAO TOTAL: 1-Carta1 | 0-Carta2 \n");
+    numPopulacaoV = numPopulacao1 > numPopulacao2;
+    printf(">>> %i <<<\n", (int)numPopulacaoV);
+    printf("Vencedor do atributo AREA TOTAL: 1-Carta1 | 0-Carta2 \n");
+    areV = area1 > area2;
+    printf(">>> %i <<<\n", (int)areV);
+    printf("Vencedor do atributo PIB TOTAL: 1-Carta1 | 0-Carta2 \n");
+    pibV = pib1 > pib2;
+    printf(">>> %i <<<\n", (int)pibV);
+    printf("Vencedor do atributo PONTUACAO TURISTICA: 1-Carta1 | 0-Carta2 \n");
+    pontoTV = pontoT1 > pontoT1;
+    printf(">>> %i <<<\n", (int)pontoTV);
+    printf("Vencedor do atributo DENSIDADE POPULACIONAL: 1-Carta1 | 0-Carta2 \n");
+    densPopulacionalV = (1/densPopulacional1) > (1/densPopulacional2);
+    printf(">>> %i <<<\n", (int)densPopulacionalV);
+    printf("Vencedor do atributo PIB PER CAPITA: 1-Carta1 | 0-Carta2 \n");
+    pibpercV = pibperc1 > pibperc2;
+    printf(">>> %i <<<\n", (int)pibpercV);
+    printf("Vencedor do atributo SUPER PODER!!!!): 1-Carta1 | 0-Carta2 \n");
+    superV = super1 > super2;
+    printf(">>> %i <<<\n", (int)superV);
+
 
 
     return 0;
